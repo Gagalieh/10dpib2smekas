e8f78daeffd6a94a46d750afdaac1f05f80e4659
Gagalieh <muhammadgalihpakuan@gmail.com>
2025-11-14 18:46:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,22 @@
-// Import the functions you need from the SDKs you need
+// config.js (ganti file lama)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-storage.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDHdq8NtG03ZON3ND6qWaHCuoOzFr7PIsU",
   authDomain: "confessdata-a824c.firebaseapp.com",
   projectId: "confessdata-a824c",
-  storageBucket: "confessdata-a824c.firebasestorage.app",
+  storageBucket: "confessdata-a824c.appspot.com",
   messagingSenderId: "1084283671185",
   appId: "1:1084283671185:web:96223afcaad4b8175f2530",
   measurementId: "G-7NHZ5GDTNT"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-// Export Firestore to use in other scripts
-export { db };
+export { db, auth, storage };
